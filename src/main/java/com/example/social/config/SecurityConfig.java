eb7bd8b79cd35e33c17af661b70ed7a3af01f6bd
Ezra Lim <skywno@gmail.com>
2022-11-01 17:23:45 +0200

    Implement OAuth2UserService
    this is to connect oauth2 and social-app sign-up.
    if a user is new, then add to the database. 
    if the user is already registered, then does not add to the database.

--- src/main/java/com/example/social/config/SecurityConfig.java
+++ src/main/java/com/example/social/config/SecurityConfig.java
@@ -3,15 +3,19 @@ package com.example.social.config;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
@@ -19,10 +23,13 @@ import java.util.List;
 
 @AllArgsConstructor
 @Configuration
+@PropertySource("classpath:registrations.properties")
 @EnableWebSecurity
 public class SecurityConfig {
     private final Environment environment;
     private final String registration = "spring.security.oauth2.client.registration.";
+    private final FacebookOAuth2UserService facebookOAuth2UserService;
+    private final GoogleOAuth2UserService googleOAuth2UserService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,6 +39,10 @@ public class SecurityConfig {
         ).oauth2Login(httpSecurityOAuth2LoginConfigurer -> httpSecurityOAuth2LoginConfigurer
                 .clientRegistrationRepository(clientRegistrationRepository())
                 .authorizedClientService(authorizedClientService())
+                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                        .oidcUserService(googleOAuth2UserService) // google 인증, OpenID Connect 1.0
+                        .userService(facebookOAuth2UserService)   // facebook 인증, OAuth2 통신
+                )
         );
 
         return http.build();
@@ -43,7 +54,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    private ClientRegistrationRepository clientRegistrationRepository() {
+    public ClientRegistrationRepository clientRegistrationRepository() {
         final List<ClientRegistration> clientRegistrations = Arrays.asList(
                 googleClientRegistration(),
                 facebookClientRegistration()
@@ -53,10 +64,8 @@ public class SecurityConfig {
     }
 
     private ClientRegistration facebookClientRegistration() {
-        final String clientId = environment.getProperty(registration + "facebook" +
-                ".client-id");
-        final String clientSecret = environment.getProperty(registration + "facebook" +
-                ".client-secret");
+        final String clientId = environment.getProperty("facebook.client-id");
+        final String clientSecret = environment.getProperty("facebook.client-secret");
 
         return CommonOAuth2Provider.FACEBOOK
                 .getBuilder("facebook")
@@ -72,10 +81,8 @@ public class SecurityConfig {
     }
 
     private ClientRegistration googleClientRegistration() {
-        final String clientId = environment.getProperty(registration + "google" +
-                ".client-id");
-        final String clientSecret = environment.getProperty(registration + "google" +
-                ".client-secret");
+        final String clientId = environment.getProperty("google.client-id");
+        final String clientSecret = environment.getProperty("google.client-secret");
 
         return CommonOAuth2Provider
                 .GOOGLE
